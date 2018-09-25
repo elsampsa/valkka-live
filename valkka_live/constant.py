fa5e92b2aba947e4eae1773ac63bcf7930979b35
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2018-09-25 13:47:05 +0300

    debugged setup.py

--- valkka_live/constant.py
+++ valkka_live/constant.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License along w
 @file    constant.py
 @author  Sampsa Riikonen
 @date    2018
-@version 0.1.0 
+@version 0.1.1 
 @brief   Constant strings, etc.
 """
 
