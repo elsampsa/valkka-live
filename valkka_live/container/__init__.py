7700fedeab75b986efd614e08cc1402a9f0d3c2f
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2018-10-03 09:57:46 +0300

    no crash if mvision absent

--- valkka_live/container/__init__.py
+++ valkka_live/container/__init__.py
@@ -1,6 +1,10 @@
 __all__ = ["video", "mvision", "root", "grid"]
 
 from .video import *
-from .mvision import *
 from .root import *
 from .grid import * 
+
+try:
+    from .mvision import *
+except ModuleNotFoundError:
+    print("valkka_live : no machine vision plugin found")
